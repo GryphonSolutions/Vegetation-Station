9414529489f7a6ef665e74a654e888a1e055c04b
akbarimo <mo.akbari15@gmail.com>
2023-01-14 13:40:59 -0800

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,10 @@
-// web/webpack.config.js
-
 const path = require('path');
 const webpack = require('webpack');
 
 const appDirectory = path.resolve(__dirname, './client');
-console.log(appDirectory, 'APP DIR');
 
-// This is needed for webpack to compile JavaScript.
-// Many OSS React Native packages are not compiled to ES5 before being
-// published. If you depend on uncompiled packages they may cause webpack build
-// errors. To fix this webpack can be configured to compile to the necessary
-// `node_module`.
 const babelLoaderConfiguration = {
   test: /\.(js|jsx)$/,
-  // Add every directory that needs to be compiled by Babel during the build.
   use: {
     loader: 'babel-loader',
     options: {
@@ -23,7 +14,6 @@ const babelLoaderConfiguration = {
         '@babel/preset-env',
         '@babel/preset-react',
       ],
-      // Re-write paths to import only the modules needed by the app
       plugins: [
         'react-native-web',
         ['@babel/plugin-proposal-private-methods', { loose: true }],
@@ -34,7 +24,6 @@ const babelLoaderConfiguration = {
   },
 };
 
-// This is needed for webpack to import static images in JavaScript files.
 const imageLoaderConfiguration = {
   test: /\.(gif|jpe?g|png|svg)$/,
   use: {
@@ -47,33 +36,19 @@ const imageLoaderConfiguration = {
 };
 
 module.exports = {
-  entry: [
-    // load any web API polyfills
-    // path.resolve(appDirectory, 'polyfills-web.js'),
-    // your web-specific entry file
-    path.join(appDirectory, '/src/index.jsx'),
-  ],
-
-  // configures where the build ends up
+  entry: [path.join(appDirectory, '/src/index.jsx')],
   output: {
     filename: 'bundle.js',
     path: path.join(appDirectory, '/public/bundle.js'),
   },
-
-  // ...the rest of your config
-
   module: {
     rules: [babelLoaderConfiguration, imageLoaderConfiguration],
   },
   mode: 'development',
   resolve: {
-    // This will only alias the exact import "react-native"
     alias: {
       'react-native$': 'react-native-web',
     },
-    // If you're working on a multi-platform React Native app, web-specific
-    // module implementations should be written in files using the extension
-    // `.web.js`.
     extensions: ['.web.js', '.js', '.jsx'],
   },
 };
