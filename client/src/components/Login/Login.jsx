aeaf1a95b1a61c86e2f495bd98c58bf33f15f23b
Kyle Martinelli <kylelmartinelli@gmail.com>
2023-01-16 18:40:56 -0800

    login skeleton complete

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Separator,
+} from 'react-native';
+import styles from './assets/StyleSheet.jsx';
 
 const Login = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login</Text>
+      <View>
+        <Text style={styles.appTitle}>Vegetation Station</Text>
+      </View>
+      <TextInput placeholder="email" style={styles.loginInputs} />
+      <TextInput
+        placeholder="password"
+        secureTextEntry="true"
+        style={styles.loginInputs}
+      />
+      <View style={styles.loginButtonContainer}>
+        <Button title="Login" color="grey" style={styles.loginButtons} />
+        <Button title="Register" styles={styles.loginButtons} />
+      </View>
     </View>
   );
 };
-
 export default Login;
