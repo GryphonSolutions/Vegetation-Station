4ed09809dee8bf19f02a71508af8dab364c8eb56
JustDatGuy <BAnusiem@gmail.com>
2023-01-17 22:27:00 -0800

    Removed sample data from Submit

--- client/src/components/Trades/Submit/Submit.jsx
+++ client/src/components/Trades/Submit/Submit.jsx
@@ -3,7 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { data } from '../../../../../server/data/exampleData.js';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,15 +18,15 @@ const Submit = () => {
   const [value, setValue] = useState([]);
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    const plantNames = data.map((plant) => {
-      return {
-        label: plant['Latin name'],
-        value: plant,
-      };
-    });
-    setItems(plantNames);
-  }, []);
+  // useEffect(() => {
+  //   const plantNames = data.map((plant) => {
+  //     return {
+  //       label: plant['Latin name'],
+  //       value: plant,
+  //     };
+  //   });
+  //   setItems(plantNames);
+  // }, []);
 
   const testButton = () => {
     Alert.alert(`The sample data name is: ${value['Latin name']}`);
@@ -42,14 +41,14 @@ const Submit = () => {
         color="#841584"
         accessibilityLabel="This is only a test button"
       />
-      <DropDownPicker
+      {/* <DropDownPicker
         open={open}
         value={value}
         items={items}
         setOpen={setOpen}
         setValue={setValue}
         setItems={setItems}
-      />
+      /> */}
 
       <StatusBar style="auto" />
     </View>
