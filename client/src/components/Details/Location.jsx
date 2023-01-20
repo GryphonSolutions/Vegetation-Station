383b2f70bc6be0bdfd799f65f865ef2ca9190b22
Thomas Saldana <saldana.tom@gmail.com>
2023-01-20 10:39:04 -0800

    fix dropdown menus for post

--- client/src/components/Details/Location.jsx
+++ client/src/components/Details/Location.jsx
@@ -1,16 +1,16 @@
-// import React, { useState } from 'react';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { StatusBar } from 'expo-status-bar';
-// // import GOOGLE_API_KEY from 'dotenv';
-// import MapView, {
-//   Marker,
-//   Callout,
-//   Circle,
-//   PROVIDER_GOOGLE,
-// } from 'react-native-maps';
-// import { Image, StyleSheet, Text, View, Button } from 'react-native';
-// import styles from './assets/StyleSheet';
+import React, { useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useDispatch, useSelector } from 'react-redux';
+import { StatusBar } from 'expo-status-bar';
+// import GOOGLE_API_KEY from 'dotenv';
+import MapView, {
+  Marker,
+  Callout,
+  Circle,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps';
+import { Image, StyleSheet, Text, View, Button } from 'react-native';
+import styles from './assets/StyleSheet';
 
 const Location = ({ coordinates }) => {
   const { selectedUser } = useSelector((state) => state.data);
@@ -25,14 +25,7 @@ const Location = ({ coordinates }) => {
           latitudeDelta: 0.04,
           longitudeDelta: 0.05,
         }}
-        // provider={PROVIDER_GOOGLE}
       >
-        {/* <Marker
-            coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-            }}
-          /> */}
         <Circle
           center={{
             latitude: location.latitude,
@@ -53,4 +46,4 @@ const Location = ({ coordinates }) => {
   );
 };
 
-// export default Location;
+export default Location;
