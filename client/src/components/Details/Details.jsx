ea112ec4e0bd4f3933ef994be7419b6dd5e3b374
Matt <siglerm37@gmail.com>
2023-01-19 21:32:29 -0800

    Fixing stylings

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -23,62 +23,72 @@ const navigateSelectedProfile = () => {
 
 const Details = () => {
   const { selectedUser, currentPlant } = useSelector((state) => state.data);
+  const { isDarkMode } = useSelector((state) => state.app);
   const { description, images, preferredTrade, commonName, postTitle } =
     currentPlant;
   const dispatch = useDispatch();
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <ScrollView style={{ marginHorizontal: '8%', flex: 1 }}>
-        <View style={styles.imageContainer}>
-          <Image
-            style={styles.plantPic}
-            source={{ uri: currentPlant.images[0] }}
-          />
-        </View>
-        <View style={styles.postContainer}>
-          <Text style={styles.postTitle}>{postTitle}</Text>
-          <View style={styles.plantNameContainer}>
-            <Text style={styles.plantNameText}>{commonName}</Text>
-          </View>
-          <View style={styles.postDescContainer}>
-            <View style={styles.prefTradesContainer}>
-              <Text style={styles.descTradesText}>Preferred trades: </Text>
-              <Text style={styles.prefTradeText}>{preferredTrade}</Text>
+    <View
+      style={{ flex: 1, backgroundColor: isDarkMode ? '#141312' : '#f0f4f1' }}
+    >
+      <SafeAreaView style={{ flex: 0 }} />
+      <View style={{ flex: 1 }}>
+        <Header />
+        <View style={styles.contentContainer}>
+          <ScrollView>
+            <View style={styles.plantImageContainer}>
+              <Image
+                style={styles.plantImage}
+                source={{ uri: currentPlant.images[0] }}
+              />
             </View>
-            <Text style={styles.plantPostDesc}>{description}</Text>
+            <View style={styles.postContainer}>
+              <Text style={styles.postTitle}>{postTitle}</Text>
+              <View style={styles.plantNameContainer}>
+                <Text style={styles.plantNameText}>{commonName}</Text>
+              </View>
+              <View style={styles.postDescContainer}>
+                <View style={styles.prefTradesContainer}>
+                  <Text style={styles.descTradesText}>Preferred trades: </Text>
+                  <Text style={styles.prefTradeText}>{preferredTrade}</Text>
+                </View>
+                <Text style={styles.plantPostDesc}>{description}</Text>
+              </View>
+            </View>
+            <View style={styles.LocationContainer}>
+              <Text style={styles.plantNameText}>
+                Trader's General Location:
+              </Text>
+              <Location />
+            </View>
+          </ScrollView>
+          <TouchableOpacity onPress={() => navigateSelectedProfile()}>
+            <ProfileRibbon />
+          </TouchableOpacity>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.detailPageButton}>
+              <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.detailPageButton}
+              onPress={() => {
+                navigation.navigate('Post');
+              }}
+            >
+              <Text style={styles.buttonText}>Propose Trade</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.detailPageButton}
+              onPress={() => {
+                navigation.navigate('Messages');
+              }}
+            >
+              <Text style={styles.buttonText}>Message</Text>
+            </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.LocationContainer}>
-          <Text style={styles.plantNameText}>Trader's General Location:</Text>
-          <Location />
-        </View>
-      </ScrollView>
-      <TouchableOpacity onPress={() => navigateSelectedProfile()}>
-        <ProfileRibbon />
-      </TouchableOpacity>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.detailPageButton}>
-          <Text style={styles.buttonText}>Save</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.detailPageButton}
-          onPress={() => {
-            navigation.navigate('Post');
-          }}
-        >
-          <Text style={styles.buttonText}>Propose Trade</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.detailPageButton}
-          onPress={() => {
-            navigation.navigate('Messages');
-          }}
-        >
-          <Text style={styles.buttonText}>Message</Text>
-        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
